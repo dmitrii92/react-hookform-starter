d29331bfb639e33cde22ee72db27c7d03fc460e6
dmitrii92 <fogot92@gmail.com>
2020-04-14 17:42:39 +0300

    Updates

---
+++ precache-manifest.0e765aaa6c750c806f62585efd614a7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3daa37d52855a9f70aee8eeb71b48332",
+    "url": "/react-hookform-starter/index.html"
+  },
+  {
+    "revision": "f7d7290b27d76130e631",
+    "url": "/react-hookform-starter/static/css/main.8c21a527.chunk.css"
+  },
+  {
+    "revision": "65abee6c7d42f53cde76",
+    "url": "/react-hookform-starter/static/js/2.8fff5d74.chunk.js"
+  },
+  {
+    "revision": "e468b20662ce7acec401182a9064b329",
+    "url": "/react-hookform-starter/static/js/2.8fff5d74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7d7290b27d76130e631",
+    "url": "/react-hookform-starter/static/js/main.f30525b8.chunk.js"
+  },
+  {
+    "revision": "e78441d93c82ff30ce37",
+    "url": "/react-hookform-starter/static/js/runtime-main.79a8ea62.js"
+  }
+]);
